a3e7b7b07b9c72b1530fb206491353fcef6e540a
Ryan Walker <ryanwalker55@gmail.com>
2020-01-15 13:45:27 -0800

    References with React components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,18 @@ import withClass from '../../../HOC/WithClass';
 
 
 class Person extends Component{
+
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    //selects the last input that rendors
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render(){
         console.log("Person.js Rendering...");
         return (
@@ -15,7 +27,13 @@ class Person extends Component{
             <Aux>
                 <p key="i1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old ! {this.props.children}</p>
                 <p key='i2'>{this.props.children}</p>    
-                <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input 
+                    key='i3' 
+                    // ref={(inputEl)=>{this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}/>
             </Aux>
         );       
             
